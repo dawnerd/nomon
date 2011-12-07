3d4095db799c363a351544b66861b2622d0eb1f2
Troy Whiteley <dawnerd@gmail.com>
2011-12-07 11:55:47 -0800

    Added defaults.

--- nomon.js
+++ nomon.js
@@ -8,8 +8,9 @@
 (function($){
   $.fn.nomon = function nomon(opts) {
     var parent = this,
+        opts = opts || {},
         words = this.text().split(' ');
-    words.splice(opts.words, 1, '<span style="display: none" data-action-receiver="toggleHiddenText">');
+    words.splice(opts.words || 20, 1, '<span style="display: none" data-action-receiver="toggleHiddenText">');
     words.push('</span>');
     words.push('<a href="#" data-action="toggleHiddenText">Read More</a>')
 
